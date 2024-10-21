4a96b3a9ddec9c2b3a7802f4bc94f60f14bccd49
Robson Lopes Cavalcante <roblucci100@gmail.com>
2024-10-21 11:12:32 -0300

    Refactor: redundant conditionals removed

--- index.js
+++ index.js
@@ -36,17 +36,17 @@ function addXp(xp) {
     heroElo = 'Unranked'
   } else if(totalHeroXp < 1000) {
     heroElo = 'Ferro'
-  } else if (totalHeroXp > 1000 && totalHeroXp <= 2000) {
+  } else if (totalHeroXp <= 2000) {
     heroElo = 'Bronze'
-  } else if (totalHeroXp > 2000 && totalHeroXp <= 5000) {
+  } else if (totalHeroXp <= 5000) {
     heroElo = 'Prata'
-  } else if (totalHeroXp > 5000 && totalHeroXp <= 7000) {
+  } else if (totalHeroXp <= 7000) {
     heroElo = 'Ouro'
-  } else if (totalHeroXp > 7000 && totalHeroXp <= 8000) {
+  } else if (totalHeroXp <= 8000) {
     heroElo = 'Platina'
-  } else if (totalHeroXp > 8000 && totalHeroXp <= 9000) {
+  } else if (totalHeroXp <= 9000) {
     heroElo = 'Ascendente'
-  } else if (totalHeroXp > 9000 && totalHeroXp <= 1000) {
+  } else if (totalHeroXp <= 1000) {
     heroElo = 'Imortal'
   } else {
     heroElo = 'Radiante'
